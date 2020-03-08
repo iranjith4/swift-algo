bdfbe8d84683c1ec025ff2e1cf9fffe8ef6a7534
Ranjithkumar Matheswaran <r@ir4.in>
2020-03-09 01:45:48 +0530

    Printing nodes

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -1,3 +1,47 @@
 import UIKit
 
-var str = "Hello, playground"
+class Node {
+    var value: String
+    var next: Node?
+    weak var previous: Node?
+    
+    init(value: String) {
+        self.value = value
+    }
+}
+
+class LinkedList {
+    var head: Node?
+    var tail: Node?
+    
+    //Append LinkedList
+    func append(value: String) {
+        let new = Node(value: value)
+        if let tail = tail {
+            tail.next = new
+            new.previous = tail
+        } else {
+            head = new
+        }
+        tail = new
+    }
+    
+    func printNodes() {
+        var printingNode = head
+        while printingNode != nil {
+            print(printingNode!.value)
+            printingNode = printingNode?.next
+        }
+    }
+}
+
+let a = LinkedList()
+a.append(value: "Mercedes")
+a.append(value: "Ferrari")
+a.append(value: "Redbull")
+a.append(value: "Mclaren")
+a.append(value: "Renault")
+
+a.printNodes()
+
+
